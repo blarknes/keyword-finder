9514af29482eb918a65c4d633210631fb3443cee
blarknes <blarknes@gmail.com>
2025-07-28 04:46:31 -0300

    fix: make string args final

--- src/main/java/blarknes/keywordfinder/Main.java
+++ src/main/java/blarknes/keywordfinder/Main.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
