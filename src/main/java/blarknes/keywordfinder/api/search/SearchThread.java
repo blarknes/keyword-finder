92a17870c5d3a8be5559f8982dd3dce988fc89f8
blarknes <blarknes@gmail.com>
2025-07-28 03:25:55 -0300

    fix: change deprecated URL to URI

--- src/main/java/blarknes/keywordfinder/api/search/SearchThread.java
+++ src/main/java/blarknes/keywordfinder/api/search/SearchThread.java
@@ -3,7 +3,7 @@ package blarknes.keywordfinder.api.search;
 import static java.util.regex.Pattern.CASE_INSENSITIVE;
 
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
 import java.util.regex.Pattern;
 
 import blarknes.keywordfinder.api.search.model.SearchInformation;
@@ -25,7 +25,8 @@ public class SearchThread implements Runnable {
     private final String currentUrl;
     private final HttpRequestSender httpRequestSender;
 
-    private static final Pattern URL_REGEX_PATTERN = Pattern.compile("<a\\s+(?:[^>]*?\\s+)?href=([\"'])((?!\").*?)\\1", CASE_INSENSITIVE);
+    private static final Pattern URL_REGEX_PATTERN = Pattern.compile("<a\\s+(?:[^>]*?\\s+)?href=([\"'])((?!\").*?)\\1",
+            CASE_INSENSITIVE);
 
     /**
      * The runnable method to search for the desired informations on the web page.
@@ -73,7 +74,7 @@ public class SearchThread implements Runnable {
         val baseUrl = this.information.getBaseurl();
 
         try {
-            return new URL(new URL(baseUrl), foundUrl).toString();
+            return URI.create(baseUrl).resolve(foundUrl).toURL().toString();
         } catch (MalformedURLException e) {
             log.warn("There was an error building the url " + foundUrl);
             return null;
